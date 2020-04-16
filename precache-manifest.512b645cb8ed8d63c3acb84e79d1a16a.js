6f8ffe5ed3853168eb15edc24f3d9ab04f34c011
themiee <navyblack16@yahoo.com>
2020-04-16 19:14:51 +0100

    Updates

---
+++ precache-manifest.512b645cb8ed8d63c3acb84e79d1a16a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "833a8c04171a944e1f80",
+    "url": "/LightsOut/static/css/main.048f1bf9.chunk.css"
+  },
+  {
+    "revision": "833a8c04171a944e1f80",
+    "url": "/LightsOut/static/js/main.833a8c04.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/LightsOut/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "135e11432d80503c488c",
+    "url": "/LightsOut/static/js/runtime~main.135e1143.js"
+  },
+  {
+    "revision": "ea87007178b1a3a7d29ee2c155a4ec04",
+    "url": "/LightsOut/index.html"
+  }
+];
